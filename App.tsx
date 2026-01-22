db9545fdb16bf804d4163c2ed806cc78b25c4532
olumudehassanololade-gigsapp <olumudehassanololade@gmail.com>
2026-01-22 22:40:54 +0100

    Create App.tsx

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>GigSwift is Live 🚀</h1>
+      <p>Welcome to your Gigs app!</p>
+    </div>
+  );
+}
+
+export default App;
